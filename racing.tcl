56b5d518235565ca03acb446cc2bd1bd17f1bed1
Edward Brekelbaum <nedbrek@yahoo.com>
2011-10-15 09:27:19 -0400

    Moved car logic into a class

--- racing.tcl
+++ racing.tcl
@@ -1,31 +1,34 @@
+#### setup
+package require XOTcl
+namespace import xotcl::*
+
 load ./sdlmix.dll
 
-set ::music 0
+#### globals
+set ::pi  [expr 4 * atan(1)]
+set ::deg [expr $pi / 180]
 
-proc toggleMusic {} {
-	if {$::music} {
-		sdl::mix::music ""
-		set ::music 0
-	} else {
-		sdl::mix::music [lindex [glob *.mod] 0]
-		set ::music 1
-	}
+#### Car class
+Class create ^Car
+
+^Car instproc init {} {
+	my instvar carAng carSpeed carX carY
+
+	set carAng   [expr 5 * $::pi / 8]
+	set carSpeed  0
+	set carX     15
+	set carY     10
 }
-toggleMusic
-
-set ::pi       [expr 4 * atan(1)]
-set ::carAng   [expr 5 * $pi / 8]
-set ::deg      [expr $pi / 180]
-
-proc reset {} {
-	set ::carSpeed  0
-	set ::carX     15
-	set ::carY     10
-}
-reset
 
 # construct a triplet of coords
-proc makeCar {r t offX offY} {
+^Car instproc makeCar {} {
+	my instvar carAng carSpeed carX carY
+
+	set r 10 ;# car "radius" (size)
+	set t $carAng
+	set offX $carX
+	set offY $carY
+
 	set ret ""
 
 	# point 1, right side
@@ -50,47 +53,79 @@ proc makeCar {r t offX offY} {
 	set cheight [.c cget -height]
 
 	if {$minX < 0} {
-		set ::carX [expr $::carX - $minX]
-		set ::carSpeed 0
+		set carX [expr $carX - $minX]
+		set carSpeed 0
 	} elseif {$maxX > $cwidth} {
-		set ::carX [expr $::carX - $maxX + $cwidth]
-		set ::carSpeed 0
+		set carX [expr $carX - $maxX + $cwidth]
+		set carSpeed 0
 	}
 
 	if {$minY < 0} {
-		set ::carY [expr $::carY - $minY]
-		set ::carSpeed 0
+		set carY [expr $carY - $minY]
+		set carSpeed 0
 	} elseif {$maxY > $cheight} {
-		set ::carY [expr $::carY - $maxY + $cheight]
-		set ::carSpeed 0
+		set carY [expr $carY - $maxY + $cheight]
+		set carSpeed 0
 	}
 
 	return $ret
 }
 
-proc incrAng {} {
-	set amt [expr (abs($::carSpeed)+3) / 4.0 * $::deg]
-	set ::carAng [expr $::carAng + $amt]
-	if {$::carAng > 2 * $::pi} {
-		set ::carAng 0
+^Car instproc move {} {
+	my instvar carX carY carSpeed carAng
+
+	# adjust position for speed
+	set carX [expr $carX + $carSpeed/4.0 * cos($carAng)]
+	set carY [expr $carY + $carSpeed/4.0 * sin($carAng)]
+}
+
+^Car instproc accel {{amt 1}} {
+	my instvar carSpeed
+	incr carSpeed $amt
+}
+
+^Car instproc incrAng {} {
+	my instvar carAng carSpeed
+
+	set amt [expr (abs($carSpeed)+3) / 4.0 * $::deg]
+	set carAng [expr $carAng + $amt]
+	if {$carAng > 2 * $::pi} {
+		set carAng 0
 	}
 }
 
-proc decrAng {} {
-	set amt [expr (abs($::carSpeed)+3) / 4.0 * $::deg]
-	set ::carAng [expr $::carAng - $amt]
-	if {$::carAng < 0} {
-		set ::carAng [expr 2 * $::pi]
+^Car instproc decrAng {} {
+	my instvar carAng carSpeed
+
+	set amt [expr (abs($carSpeed)+3) / 4.0 * $::deg]
+	set carAng [expr $carAng - $amt]
+	if {$carAng < 0} {
+		set carAng [expr 2 * $::pi]
 	}
 }
+
+#### cars
+^Car create p1car ;# player 1 car
+
+#### music
+set ::music 0
+
+proc toggleMusic {} {
+	if {$::music} {
+		sdl::mix::music ""
+		set ::music 0
+	} else {
+		sdl::mix::music [lindex [glob *.mod] 0]
+		set ::music 1
+	}
+}
+toggleMusic
 
 proc eventLoop {} {
-	# adjust position for speed
-	set ::carX [expr $::carX + $::carSpeed/4.0 * cos($::carAng)]
-	set ::carY [expr $::carY + $::carSpeed/4.0 * sin($::carAng)]
+	p1car move
 
 	# redraw car
-	.c coords $::carId [makeCar 10 $::carAng $::carX $::carY]
+	.c coords $::carId [p1car makeCar]
 
 	after 30 eventLoop
 }
@@ -99,10 +134,10 @@ pack [canvas .c] -side top -expand 1 -fill both
 
 set ::carId [.c create poly 10 0 20 10 0 20 -fill blue -tags car]
 
-bind .c <Left>  {decrAng}
-bind .c <Right> {incrAng}
-bind .c <Up>    {incr ::carSpeed}
-bind .c <Down>  {incr ::carSpeed -1}
+bind .c <Left>  {p1car decrAng}
+bind .c <Right> {p1car incrAng}
+bind .c <Up>    {p1car accel}
+bind .c <Down>  {p1car accel -1}
 bind .c <Enter> {focus %W}
 
 bind .c <m> {toggleMusic}
